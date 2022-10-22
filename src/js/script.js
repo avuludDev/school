18f984298c068a451fab6170837ac3d157e0611b
avuludDev <avulud.dev@gmail.com>
2022-10-22 03:06:32 +0300

    Mobile Version

--- src/js/script.js
+++ src/js/script.js
@@ -8,15 +8,25 @@ const swiper = new Swiper('.swiper', {
           delay: 2500,
           disableOnInteraction: false,
         },
-    slidesPerView: 2,
+    slidesPerView: 1,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-    }
-  
+    },
+    breakpoints: {
+      750: {
+        slidesPerView: 2,
+      }
+    } 
     // Navigation arrows
     
   
     // And if we need scrollbar
    
   });
+
+  const nav = document.getElementsByTagName("nav")[0];
+  nav.addEventListener("click",()=>{
+    document.querySelector("nav ul").style.display = (document.querySelector("nav ul").style.display == 'none') ? "block" : "none";
+    document.querySelector("nav .after").style.transform = (document.querySelector("nav ul").style.display == 'none') ? "translateX(-50%) rotate(45deg)" : "translateX(-50%) translateY(10px) rotate(225deg)"; 
+  })
